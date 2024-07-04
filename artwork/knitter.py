c9769b2bccdbc0435716872bae848585cc39dd4a
Mert Gör <mertgor@masscollabs.xyz>
2024-07-04 19:46:25 +0300

    artworks update

---
+++ artwork/knitter.py
@@ -0,0 +1,49 @@
+#/usr/bin/python3.7
+
+#    Knitter is a tool for amassivus suite for managing application installations
+#    Knitter is not a package manager but a script suite which lets us to easily manage installations, and scale up our installations 
+#    Knitter runs as a server and provided services to network users so we have choosen AGPLv3 or any later license fot that purpose
+#    Enjoy it! Happy hacking!
+#
+#    Copyright (C) 2020-2023 Masscollabs Services
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    For general questions please send an e-mail to core at masscollabs dot com
+#    For technical support please send an e-mail to support at masscollabs dot com
+#    For security issues and bug reports please send an e-mail to security at masscollabs dot com
+
+print ("""
+     
+     Knitter is a tool for amassivus suite for managing application installations
+     Knitter is a package manager which lets us to easily manage installations, and scale up our installations 
+     Knitter runs as a server and provided services to network users so we have choosen AGPLv3 or any later license fot that purpose
+     Enjoy it! Happy hacking!
+
+     Copyright (C) 2020-2023 Masscollabs Services
+      
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU Affero General Public License as published
+     by the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+ 
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU Affero General Public License for more details.
+ 
+     You should have received a copy of the GNU Affero General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ 
+""")
